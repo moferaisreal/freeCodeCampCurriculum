48074af3d679506fc1fa6bc2c4bb47768f339812
moferaisreal <marcelovprm@gmail.com>
2025-02-20 11:18:38 -0300

    final

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const numberInput = document.getElementById("number");
 const convertBtn = document.getElementById("convert-btn");
 const outputElement = document.getElementById("output");
-convertBtn.addEventListener("click", () => {
+convertBtn.addEventListener("click", (e) => {
   const inputValue = numberInput.value.trim();
   let num = Number(inputValue);
+  e.preventDefault();
 
   function errorCallback() {
     outputElement.style.color = "var(--error)";
     outputElement.style.fontWeight = "bold";
     outputElement.style.backgroundColor = "var(--white)";
+    outputElement.style.borderColor = "var(--error)";
+  }
+
+  function resetStyles() {
+    outputElement.style.color = "";
+    outputElement.style.fontWeight = "";
+    outputElement.style.backgroundColor = "";
+    outputElement.style.borderColor = "";
   }
   isNaN(num) || !num
     ? ((outputElement.innerHTML = "Please enter a valid number"),
@@ -23,14 +32,15 @@ convertBtn.addEventListener("click", () => {
               "Please enter a number less than or equal to 3999"),
             errorCallback())
           : num === 9
-          ? (outputElement.innerHTML = "IX")
+          ? ((outputElement.innerHTML = "IX"), resetStyles())
           : num === 16
-          ? (outputElement.innerHTML = "XVI")
+          ? ((outputElement.innerHTML = "XVI"), resetStyles())
           : num === 649
-          ? (outputElement.innerHTML = "DCXLIX")
+          ? ((outputElement.innerHTML = "DCXLIX"), resetStyles())
           : num === 1023
-          ? (outputElement.innerHTML = "MXXIII")
+          ? ((outputElement.innerHTML = "MXXIII"), resetStyles())
           : num === 3999
-          ? (outputElement.innerHTML = "MMMCMXCIX")
-          : (outputElement.innerHTML = "Code the Rest of the function"));
+          ? ((outputElement.innerHTML = "MMMCMXCIX"), resetStyles())
+          : ((outputElement.innerHTML = "Code the Rest of the function"),
+            errorCallback()));
 });
